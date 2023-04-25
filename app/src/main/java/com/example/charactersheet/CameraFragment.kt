1e3cc5244a78f1a14426957e03b7fe68c8f41261
Hamish <hamishhill1909@gmail.com>
2023-04-25 20:00:19 +0100

    managed to get dice image classification model working when mlkit model failed to identify text

--- app/src/main/java/com/example/charactersheet/CameraFragment.kt
+++ app/src/main/java/com/example/charactersheet/CameraFragment.kt
@@ -5,8 +5,10 @@ import android.content.ContentValues
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.graphics.Color
+import android.net.Uri
 import android.os.Build
 import android.os.Bundle
+import android.os.Environment
 import android.provider.MediaStore
 import android.util.Log
 import android.view.LayoutInflater
@@ -34,10 +36,15 @@ import org.tensorflow.lite.support.image.ImageProcessor
 import org.tensorflow.lite.support.image.TensorImage
 import org.tensorflow.lite.support.image.ops.Rot90Op
 import org.tensorflow.lite.task.core.BaseOptions
+import org.tensorflow.lite.task.vision.classifier.Classifications
+import org.tensorflow.lite.task.vision.classifier.ImageClassifier
 import org.tensorflow.lite.task.vision.detector.Detection
 import org.tensorflow.lite.task.vision.detector.ObjectDetector
 import org.tensorflow.lite.task.vision.detector.ObjectDetector.ObjectDetectorOptions
+import java.io.File
+import java.io.FileOutputStream
 import java.io.IOException
+import java.io.OutputStream
 import java.text.SimpleDateFormat
 import java.util.*
 import java.util.concurrent.ExecutorService
@@ -161,7 +168,6 @@ class CameraFragment : Fragment() {
 
                     val msg = "Photo capture succeeded" // : ${output.savedUri}"
                     Toast.makeText(requireContext(), msg, Toast.LENGTH_SHORT).show()
-                    Log.d(TAG, msg)
 
                     val image: InputImage
                     try {
@@ -182,8 +188,25 @@ class CameraFragment : Fragment() {
     }
 
     private fun isPixelIn(img: Bitmap, results: List<Detection>): Bitmap {
-        for (y in 0 until img.height)  //loop from 0 to img.height-1
-            for (c in 0 until img.width) {//loop from 0 to img.width-1
+
+        var cropLeft = (results[0].boundingBox.left - 500).toInt()
+        var cropTop = (results[0].boundingBox.top - 500).toInt()
+        var cropH = (results[0].boundingBox.height() + 1000).toInt()
+        var cropW = (results[0].boundingBox.width() + 1000).toInt()
+        if (cropLeft < 0)
+            cropLeft = 1
+
+        if (cropTop < 0)
+            cropTop = 1
+
+        if(cropH + cropTop > img.height)
+            cropH = img.height - cropTop
+
+        if(cropW + cropLeft > img.width)
+            cropW = img.width - cropLeft
+
+        for (y in cropTop until cropTop+cropH)  //loop from 0 to img.height-1
+            for (c in cropLeft until cropLeft+cropW) {//loop from 0 to img.width-1
                 if (results.size == 1) {//if theres only one bounding box to check against
                     val vectorAB = listOf(
                         results[0].boundingBox.right.toInt() - results[0].boundingBox.left.toInt(),
@@ -221,26 +244,12 @@ class CameraFragment : Fragment() {
                 }
             }
 
-        var cropLeft = (results[0].boundingBox.left - 500).toInt()
-        var cropTop = (results[0].boundingBox.top - 500).toInt()
-        var cropH = (results[0].boundingBox.height() + 1000).toInt()
-        var cropW = (results[0].boundingBox.width() + 1000).toInt()
-        if (cropLeft < 0)
-            cropLeft = 1
-
-        if (cropTop < 0)
-            cropTop = 1
-
-        if(cropH + cropTop > img.height)
-            cropH = img.height - cropTop
-
-        if(cropW + cropLeft > img.width)
-            cropW = img.width - cropLeft
-
         val img2 = Bitmap.createBitmap(
             img, cropLeft, cropTop,
             cropW, cropH
         )
+
+        saveMediaToStorage(img2)
         return img2
     }
 
@@ -273,8 +282,9 @@ class CameraFragment : Fragment() {
     private fun getRes(): String { return resultsString }
     private fun setRes(str: String) {
         if(!textChanged){
-        textChanged = true
-        resultsString= str
+            textChanged = true
+            resultsString= str
+            fragmentCameraBinding.resultsTextCam.text = getRes()
         }
     }
 
@@ -310,7 +320,7 @@ class CameraFragment : Fragment() {
                 // Bind use cases to camera
                 val cam = cameraProvider.bindToLifecycle(
                     this, cameraSelector, preview, imageCapture)
-                
+
             } catch(exc: Exception) {
                 Log.e(TAG, "Use case binding failed", exc)
             }
@@ -324,12 +334,57 @@ class CameraFragment : Fragment() {
             requireContext(), it) == PackageManager.PERMISSION_GRANTED
     }
 
+    fun saveMediaToStorage(bitmap: Bitmap) {
+        //Generating a file name
+        val filename = "${System.currentTimeMillis()}.jpg"
+
+        //Output stream
+        var fos: OutputStream? = null
+
+        //For devices running android >= Q
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            //getting the contentResolver
+            context?.contentResolver?.also { resolver ->
+
+                //Content resolver will process the contentvalues
+                val contentValues = ContentValues().apply {
+
+                    //putting file information in content values
+                    put(MediaStore.MediaColumns.DISPLAY_NAME, filename)
+                    put(MediaStore.MediaColumns.MIME_TYPE, "image/jpg")
+                    put(MediaStore.MediaColumns.RELATIVE_PATH, Environment.DIRECTORY_PICTURES)
+                }
+
+                //Inserting the contentValues to contentResolver and getting the Uri
+                val imageUri: Uri? =
+                    resolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues)
+
+                //Opening an outputstream with the Uri that we got
+                fos = imageUri?.let { resolver.openOutputStream(it) }
+            }
+        } else {
+            //These for devices running on android < Q
+            //So I don't think an explanation is needed here
+            val imagesDir =
+                Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES)
+            val image = File(imagesDir, filename)
+            fos = FileOutputStream(image)
+        }
+
+        fos?.use {
+            //Finally writing the bitmap to the output stream that we opened
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, it)
+            //context?.toast("Saved to Photos")
+        }
+    }
+
     private fun removeLast(){
         resultsString = resultsString.substringBeforeLast(',')
         fragmentCameraBinding.resultsTextCam.text  =resultsString
     }
 
     private fun textRecog(image: InputImage): Task<Text> {
+        Log.d(TAG, "starting thread")
         val recognizer = TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS)
         return recognizer.process(image)
     }
@@ -340,30 +395,78 @@ class CameraFragment : Fragment() {
             val img = InputImage.fromBitmap(image, x * rot)
             val y = textRecog(img)
             y.addOnSuccessListener {
+                Log.d(TAG, "$x thread finished")
                 text1 = y.result.text
                 if (text1 != "") {
-                    if (resultsString != "" )
-                        setRes("$resultsString,$text1")
+                    if (text1 == "A") //added for common case where `4` is often detected as capital `A`
+                        text1 = "4"
 
-                    else if (resultsString == "") {
-                        setRes(text1)
+                    if(text1 in (1..20).toString()){
+                        if (resultsString != "" )
+                            setRes("$resultsString,$text1")
+
+                        else if (resultsString == "") {
+                            setRes(text1)
+                        }
                     }
-                    if (x == 3 && textChanged)
+                }
+                if (x == 3){
+                    fragmentCameraBinding.imageCaptureButton.visibility = View.VISIBLE
+
+                    if (textChanged)
                         textChanged = false
 
-                    fragmentCameraBinding.resultsTextCam.text = resultsString
-                } else if (text1 == "") {
-                    Toast.makeText(
-                        requireContext(),
-                        "no text found, please try again",
-                        Toast.LENGTH_SHORT
-                    ).show()
-                    Log.d(TAG, "no text in image ")
+                    if (!textChanged){
+
+                        text1 = classifyObjects(image, rot)
+                        if(text1 in (1..6).toString()) {
+                            if (resultsString != "")
+                                setRes("$resultsString,$text1")
+                            else if (resultsString == "")
+                                setRes(text1)
+                        }
+                        else
+                            Toast.makeText(requireContext(), "No text found, please try again", Toast.LENGTH_SHORT).show()
+                    }
                 }
-                if (x == 3)
-                    fragmentCameraBinding.imageCaptureButton.visibility = View.VISIBLE
             }
         }
+    }
+
+    private fun classifyObjects (box: Bitmap, rot: Int): String {
+
+        val imageProcessor =
+            ImageProcessor.Builder()
+                .add(Rot90Op(-rot / 90))
+                .build()
+
+        val tensorImage = imageProcessor.process(TensorImage.fromBitmap(box))
+
+        val options = ImageClassifier.ImageClassifierOptions.builder()
+            .setBaseOptions(BaseOptions.builder().useGpu().build())//.setScoreThreshold(0.65F)
+            .setMaxResults(1)
+            .build()
+
+        val modelFile= "D6E20_NoAug_model2_89_fp16.tflite"
+        val modelFile1= "E10_model10_fp16.tflite"
+
+        val imageClassifier = ImageClassifier.createFromFileAndOptions(
+            context, modelFile, options
+        )
+
+  val imageClassifier1 = ImageClassifier.createFromFileAndOptions(
+            context, modelFile1, options
+        )
+
+        val results: List<Classifications> = imageClassifier.classify(tensorImage)
+        val results1: List<Classifications> = imageClassifier1.classify(tensorImage)
+
+        if(results!=null) {
+            Log.d(TAG,"results: $results")
+            Log.d(TAG,"results: $results1")
+
+        }
+        return results1[0].categories.toString()
     }
 
     companion object {
